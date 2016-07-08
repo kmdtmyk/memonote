efd793492a196f51cce870ef8fc53c4cf2f33695
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-07-08 08:04:18 +0000

    add label list

---
+++ api/controllers/LabelController.js
@@ -0,0 +1,12 @@
+/**
+ * LabelController
+ *
+ * @description :: Server-side logic for managing labels
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  index(req, res){
+    res.view()
+  },
+};
