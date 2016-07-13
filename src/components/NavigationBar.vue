c7a08bfaaca15f37947b29f7710575d46f1a548e
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-07-13 08:46:35 +0000

    add navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='ui menu inverted'>
+    <a class='item' v-link='{path: "/"}'>Home</a>
+    <a class='item' v-link='{path: "/memos"}'>Memo</a>
+    <a class='item' v-link='{path: "/labels"}'>Label</a>
+    <a class='item' v-link='{path: "/users"}'>User</a>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style >
+</style>
