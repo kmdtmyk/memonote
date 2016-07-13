87d354ced9937e80f80ba97b446165d7344498a1
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-07-13 08:57:35 +0000

    add Conatiner.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <navigation-bar></navigation-bar>
-    <router-view></router-view>
+    <container>
+      <router-view></router-view>
+    </container>
   </div>
 </template>
 
 <script>
 import NavigationBar from './components/NavigationBar'
+import Container from './components/Container'
 
 export default {
   components: {
     NavigationBar,
+    Container,
   },
 }
 </script>
