4af6b993e95992a6772a048d52c6e3aca6fac708
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-07-26 05:09:06 +0000

    crypt user password

--- api/models/User.js
+++ api/models/User.js
@@ -5,10 +5,31 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+var bcrypt = require('bcrypt')
+
 module.exports = {
 
   attributes: {
+    toJSON(){
+      var user = this.toObject()
+      delete user.password
+      return user
+    },
+  },
 
-  }
+  beforeCreate(user, cb){
+    var rounds = 10
+    bcrypt.genSalt(rounds, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if(err){
+          console.log(err)
+          cb(err)
+        }else{
+          user.password = hash
+          cb(null, user)
+        }
+      })
+    })
+  },
+
 };
-
